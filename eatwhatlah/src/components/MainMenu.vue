2d1b595422b7e6157bb9a49e23424847e2ee749e
wzhixuan67 <zx.wong.2024@computing.smu.edu.sg>
2025-10-10 13:43:52 +0800

    Removed examples from main pg

--- eatwhatlah/src/components/MainMenu.vue
+++ eatwhatlah/src/components/MainMenu.vue
@@ -15,13 +15,6 @@
 <template>   
 
     <RouterLink to="/Login/">Login</RouterLink> 
-    <RouterLink to="/Example2/">Example2</RouterLink> 
-    <RouterLink to="/Example3/">Example3</RouterLink> 
-    <RouterLink to="/Example4/">Example4</RouterLink> 
-    <RouterLink to="/Example5/">Example5</RouterLink> 
-    <RouterLink to="/Example6/">Example6</RouterLink> 
-    <RouterLink to="/Example7/">Example7</RouterLink> 
-    <RouterLink to="/Example8/">Example8</RouterLink> 
     <RouterLink to="Map">Map</RouterLink> 
 
 </template>
--- eatwhatlah/src/route/routes.js
+++ eatwhatlah/src/route/routes.js
@@ -6,13 +6,6 @@ import {
 import MainMenu from '../components/MainMenu.vue';
 import Login from '../components/Login.vue';
 import SignUp from '../components/SignUp.vue';
-import Example2 from '../components/Example2.vue';
-import Example3 from '../components/Example3.vue';
-import Example4 from '../components/Example4.vue';
-import Example5 from '../components/Example5.vue';
-import Example6 from '../components/Example6.vue';
-import Example7 from '../components/Example7.vue';
-import Example8 from '../components/Example8.vue';
 import Map from '../components/Map.vue';
 
 const history = createWebHistory()
@@ -30,34 +23,7 @@ const routes = [
     path: '/SignUp/',
     component: SignUp
   },
-  {
-    path: '/Example2/',
-    component: Example2
-  },
-  {
-    path: '/Example3/',
-    component: Example3
-  },
-  {
-    path: '/Example4/',
-    component: Example4
-  },
-  {
-    path: '/Example5/',
-    component: Example5
-  },
-  {
-    path: '/Example6/',
-    component: Example6
-  },
-  {
-    path: '/Example7/',
-    component: Example7
-  },
-  {
-    path: '/Example8/',
-    component: Example8
-  },
+
   {
     path: '/Map/',
     component: Map
