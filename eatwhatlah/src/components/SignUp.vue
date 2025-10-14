046e9227f45d0fe7d47ca3d19c8ea7b31829bdfa
wzhixuan67 <zx.wong.2024@computing.smu.edu.sg>
2025-10-15 00:48:25 +0800

    Implemented firebase authentication for SignUp.vue

--- eatwhatlah/src/components/SignUp.vue
+++ eatwhatlah/src/components/SignUp.vue
@@ -1,13 +1,45 @@
 <script>
-  const link = document.createElement('link');
-  link.rel = 'stylesheet';
-  link.href = 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css';
-  document.head.appendChild(link);
+const link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = 'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css';
+document.head.appendChild(link);
 
-  const link2 = document.createElement('link');
-  link2.rel = 'stylesheet';
-  link2.href = 'https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css';
-  document.head.appendChild(link2);
+const link2 = document.createElement('link');
+link2.rel = 'stylesheet';
+link2.href = 'https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css';
+document.head.appendChild(link2);
+
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+
+export default {
+
+    data() {
+        return {
+            email: '',
+            password: '',
+            password2: '',
+        }
+    },
+    methods: {
+        register() {
+            // Tries to register new user
+            if (this.password == this.password2) {
+                this.$router.push('/Home')
+                createUserWithEmailAndPassword(getAuth(), this.email, this.password)
+                    .then((data) => {
+                        console.log("Registration successful!")
+                    })
+                    .catch((error) => {
+                        console.log(error.code);
+                        alert(error.message);
+                    });
+            } else {
+                alert("Passwords do not match!")
+            }
+        },
+    }
+}
+
 
 </script>
 
@@ -15,57 +47,66 @@
 <!-- html  -->
 <template>
     <div id="signup-page">
-    <div class="signup border rounded p-5" >
-        <form action="">
-            <h3 class="text-dark-emphasis">Sign Up to EatWhatLa!</h3>
-            <p><small><em>Glad that you will be signing up with us!</em></small></p>
-            <div class="input-group mt-4">
-                <span class="input-group-text"><i class='bx bxs-user'></i></span>
-                <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="visible-addon" required>
-            </div>
-            <div class="input-group mt-3">
-                <span class="input-group-text"><i class='bx bxs-lock-alt'></i></span>
-                <input type="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="visible-addon" required>
-            </div>
-             <div class="input-group mt-3">
-                <span class="input-group-text"><i class='bx bxs-lock-alt'></i></span>
-                <input type="password" class="form-control" placeholder="Confirm Password" aria-label="Confirm Password" aria-describedby="visible-addon" required>
-            </div>
-            <div class="mt-3 d-flex justify-content-center">
-                <button type="submit" class="btn border bg-dark text-light">Sign Up</button>
-            </div>
-            <div class="register-link mt-2 d-flex justify-content-center">
-                <p><small><em>Back to<RouterLink to="/Login/">Login</RouterLink></em></small></p>
-            </div>
-        </form>
-    </div>
+        <div class="signup border rounded p-5">
+            <form action="">
+                <h3 class="text-dark-emphasis">Sign Up to EatWhatLa!</h3>
+                <p><small><em>Glad that you will be signing up with us!</em></small></p>
+                <div class="input-group mt-4">
+                    <span class="input-group-text"><i class='bx bxs-user'></i></span>
+                    <input type="text" class="form-control" placeholder="Email" aria-label="Username"
+                        aria-describedby="visible-addon" required v-model="email">
+                </div>
+                <div class="input-group mt-3">
+                    <span class="input-group-text"><i class='bx bxs-lock-alt'></i></span>
+                    <input type="password" class="form-control" placeholder="Password (at least 6 chars)" aria-label="Password"
+                        aria-describedby="visible-addon" required v-model="password">
+                </div>
+                <div class="input-group mt-3">
+                    <span class="input-group-text"><i class='bx bxs-lock-alt'></i></span>
+                    <input type="password" class="form-control" placeholder="Confirm Password"
+                        aria-label="Confirm Password" aria-describedby="visible-addon" required v-model="password2">
+                </div>
+
+                <div id="pwMismatch" class="text-danger mt-3" v-if="password != password2">
+                    Please make sure your passwords match
+                </div>
+                <div class="mt-3 d-flex justify-content-center">
+                    <button type="submit" class="btn border bg-dark text-light" @click="register">Submit</button>
+                </div>
+                <div class="mt-3 d-flex justify-content-center">
+                    <button type="submit" class="btn border bg-dark text-light" @click="registerGoogle">Sign up with
+                        Google</button>
+                </div>
+                <div class="register-link mt-2 d-flex justify-content-center">
+                    <p><small><em>Back to<RouterLink to="/Login/">Login</RouterLink></em></small></p>
+                </div>
+            </form>
+        </div>
     </div>
 </template>
 
 
 <!-- css -->
 <style scoped>
-
-small, em, a {
-  display: inline;
+small,
+em,
+a {
+    display: inline;
 }
 
 
 #signup-page {
-  margin: 0;
-  display: flex;
-  place-items: center;
-  min-width: 320px;
-  min-height: 100vh;
-  justify-content: center; 
-  align-items: center; 
-  text-align: center;
-  padding: 2rem;
-  max-width: 1280px;
-  margin-left: auto;
-  margin-right: auto;
+    margin: 0;
+    display: flex;
+    place-items: center;
+    min-width: 320px;
+    min-height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    padding: 2rem;
+    max-width: 1280px;
+    margin-left: auto;
+    margin-right: auto;
 }
-
-
-
 </style>
--- eatwhatlah/src/main.js
+++ eatwhatlah/src/main.js
@@ -3,6 +3,25 @@ import router from './route/routes.js' // for routing
 // import './style.css'
 import App from './App.vue'
 
+// firebase
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyASQJECionJWIsNmA5Ik4oQ-sD94wuYAEY",
+  authDomain: "eatwhatlah-b6d02.firebaseapp.com",
+  databaseURL: "https://eatwhatlah-b6d02-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "eatwhatlah-b6d02",
+  storageBucket: "eatwhatlah-b6d02.firebasestorage.app",
+  messagingSenderId: "1059550584917",
+  appId: "1:1059550584917:web:bbc0dae4c9d7592d3474dc"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 const app = createApp(App)
 app.use(router).mount('#app')
